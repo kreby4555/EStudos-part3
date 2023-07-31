9876da6a41907c6642ccd20a70415373463e3f7c
kreby455 <clebinhoalves2011@gmail.com>
2023-07-31 17:46:16 -0300

    Primeiro estudo

---
+++ 1- Math.js
@@ -0,0 +1,31 @@
+/*
+
+Math -> Matemática
+
+- pow (2,2) / potência / 2 elevado a 2 = 4 / power
+- sqrt(25,2) / raiz/ raiz de 25
+- PI
+- ceil / teto -> arredondar para cima
+- floor / chão -> arredondar pra baixo
+
+- random() / número aleatório entre 0 e 1
+
+*/
+
+const result = Math.pow(2,2)
+console.log(result)
+
+const squrt = Math.sqrt(36,2)
+console.log(squrt)
+
+const PI = Math.PI
+console.log(PI)
+
+const ceil = Math.ceil(20.8)
+console.log(ceil)
+
+const floor = Math.floor(45.8)
+console.log(floor)
+
+const random = Math.random()
+console.log(random)
